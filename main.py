b40bfdc58d18bc15973a43e48b99f0e8c0864d19
kaitlynstark <kaitlyn.stark@bsu.edu>
2025-06-09 17:45:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from api_utils import fetch_wikipedia_data
 from output_utils import process_wikipedia_response
 
 def main():
-    article_titles = get_article_title_from_command_line()
-    data = fetch_wikipedia_data(article_titles)
+    article_title = get_article_title_from_command_line()
+    data = fetch_wikipedia_data(article_title)
     process_wikipedia_response(data)
 
 if __name__ == "__main__":
